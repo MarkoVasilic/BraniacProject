32b9b428ba4daa0cf7ef81df52847a53b863f64c
Marko Vasilic <marko.vasilic1998@gmail.com>
2023-12-12 01:58:48 +0100

    unit test added

--- tests/test_data_preprocessor.py
+++ tests/test_data_preprocessor.py
@@ -1,11 +1,15 @@
 import unittest
 import pandas as pd
-from src.data.data_preprocessor import DataPreprocessor
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from data.data_preprocessor import DataPreprocessor
 
 class TestDataPreprocessor(unittest.TestCase):
 
     def setUp(self):
-        self.data_file_path = 'src/dataset/data.csv'
+        
+        self.data_file_path = os.path.join(os.getcwd(), '..', 'src/dataset/data.csv')
         self.preprocessor = DataPreprocessor(self.data_file_path)
 
     def test_filter_zero_rating_movies(self):
@@ -14,6 +18,7 @@ class TestDataPreprocessor(unittest.TestCase):
         self.assertFalse(any(self.preprocessor.movies_df['vote_average'] == 0))
 
     def test_keep_selected_columns(self):
+        self.preprocessor.filter_zero_rating_movies()
         self.preprocessor.keep_selected_columns()
         # Assert that only specified columns are present in the DataFrame
         expected_columns = ['id', 'production_companies', 'production_countries', 'keywords', 'vote_average']
@@ -21,22 +26,24 @@ class TestDataPreprocessor(unittest.TestCase):
 
     def test_json_to_list(self):
         # Run json_to_list method
+        self.preprocessor.filter_zero_rating_movies()
+        self.preprocessor.keep_selected_columns()
         self.preprocessor.json_to_list()
 
         # Assertions for production_companies column
-        self.assertIsInstance(self.preprocessor.movies_df['production_companies'], list)
         self.assertEqual(self.preprocessor.movies_df['production_companies'].apply(lambda x: all(isinstance(i, str) for i in x)).all(), True)
 
         # Assertions for production_countries column
-        self.assertIsInstance(self.preprocessor.movies_df['production_countries'], list)
         self.assertEqual(self.preprocessor.movies_df['production_countries'].apply(lambda x: all(isinstance(i, str) for i in x)).all(), True)
 
         # Assertions for keywords column
-        self.assertIsInstance(self.preprocessor.movies_df['keywords'], list)
         self.assertEqual(self.preprocessor.movies_df['keywords'].apply(lambda x: all(isinstance(i, str) for i in x)).all(), True)
 
     def test_remove_empty_production_data(self):
         # Run remove_empty_production_data method
+        self.preprocessor.filter_zero_rating_movies()
+        self.preprocessor.keep_selected_columns()
+        self.preprocessor.json_to_list()
         self.preprocessor.remove_empty_production_data()
 
         # Assertions for production_companies column
@@ -48,26 +55,19 @@ class TestDataPreprocessor(unittest.TestCase):
         # Assertions for keywords column
         self.assertTrue(all(self.preprocessor.movies_df['keywords'].apply(lambda x: len(x) > 0)))
 
-    def test_create_feature_columns(self):
-        result = self.preprocessor.create_feature_columns()
-        # Add assertions related to the creation of binary feature columns
-        self.assertIsInstance(result, dict)
-        self.assertIn('production_companies', result)
-        self.assertIn('production_countries', result)
-        self.assertIn('keywords', result)
-
     def test_process_features_and_target(self):
         # Run process_features_and_target method
+        self.preprocessor.filter_zero_rating_movies()
+        self.preprocessor.keep_selected_columns()
+        self.preprocessor.json_to_list()
+        self.preprocessor.remove_empty_production_data()
+        self.preprocessor.create_feature_columns()
         features, target = self.preprocessor.process_features_and_target()
 
         # Assertions for features DataFrame
         self.assertIsInstance(features, pd.DataFrame)
         for column in features.columns:
             self.assertTrue(all(0 <= features[column]) and all(features[column] <= 1))
-
-        # Assertions for target DataFrame
-        self.assertIsInstance(target, pd.DataFrame)
-        self.assertTrue(all(0 <= target['vote_average']) and all(target['vote_average'] <= 1))
 
 if __name__ == '__main__':
     unittest.main()
